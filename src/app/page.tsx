24c6fd6e6c0223a4ff6d3dc440c094cdbc355196
ademNr <ademnr2@gmail.com>
2025-11-10 16:48:27 +0100

    remove wrong import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from '@/components/Hero';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
 
-import CVSection from '@/components/CVSection';
 
 export default function Home() {
   return (
